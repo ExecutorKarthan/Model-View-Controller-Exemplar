bedd11a85e0e6eba49bd68bf0e63c0caafd679b8
Alex Messina <me@alexmessina.dev>
2024-01-04 10:49:15 -0600

    Interface Design -Edited handlebars files to create needed user Interface -Created models to handle data input -Edited connect config -Added server script to run the server

---
+++ server.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const hbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
