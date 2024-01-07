21696ec02490031c02d335c34472daeaf8596c6f
Alex Messina <me@alexmessina.dev>
2024-01-06 23:09:46 -0600

    Added helper for date formatting

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
+const helpers = require('./utilities/helpers');
 const sequelize = require('./config/connection');
-
-
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -28,7 +26,7 @@ const sess = {
 
 app.use(session(sess));
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({helpers});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
